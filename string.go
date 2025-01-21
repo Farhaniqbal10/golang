0b941cf7e12d4e24ad7b81210f503f9d3ecf132a
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-21 10:50:34 +0700

    Update string.go
    konversi tipe data string

--- string.go
+++ string.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main(){
+
+	var name = "farhan iqbal"
+	var e = name[1]
+	var eString = string(e)
+
+	fmt.Println(name)
+	fmt.Println(eString)
+	
 	fmt.Println("Farhan")
 	fmt.Println("Maulana")
 	fmt.Println("Iqbal")
