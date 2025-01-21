b665efc009303db7b3f33f72e2dcbdd34bb5623e
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-21 11:29:29 +0700

    Create matematika.go

---
+++ matematika.go
@@ -0,0 +1,19 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+	//operator matematika
+
+	var a = 10
+	var b = 5
+	var c = 20
+	var d = 15
+	var e = 2
+	var f = 1
+	var g = a + b - c * d / e % f
+	
+	fmt.Println(g)
+
+  }
