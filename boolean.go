3b75d2869664d67b34ed33e2197758c8bb2139ba
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-21 11:35:38 +0700

    Update boolean.go

--- boolean.go
+++ boolean.go
@@ -5,4 +5,14 @@ import "fmt"
 func main(){
 	fmt.Println("benar =", true)
 	fmt.Println("salah =", false)
+
+	var nilaiakhir = 90
+	var absensi = 80
+
+	var lulusnilaiakhir bool = nilaiakhir > 80
+	var lulusabsensi bool = absensi > 80
+
+	var lulus bool = lulusnilaiakhir && lulusabsensi
+
+	fmt.Println(lulus)
 }
