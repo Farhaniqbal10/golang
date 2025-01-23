c5040f8c6caa9f4c394efcc9eedb07b22827a228
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-23 13:17:12 +0700

    Update dataslice.go

--- dataslice.go
+++ dataslice.go
@@ -2,7 +2,7 @@ package main
 import "fmt" 
 
 func main() {
-  names := [...] String {"farhan", "maulana", "iqbal", "mcgilly", "cuddy"}
+  names := [...] string {"farhan", "maulana", "iqbal", "mcgilly", "cuddy"}
   slice := names[1:3] 
 
   fmt.Println(slice[0])
@@ -16,5 +16,17 @@ func main() {
 
   slice3 := names[:]
   fmt.Println(slice3)
+
+  //append
+  days := [...] string {"senin","selasa","rabu","kamis","jumat","sabtu,","minggu"}
+  dayslice1 := days[5:]
+  dayslice1[0] = "sabtu baru"
+  dayslice1[1] = "minggu baru"
+  fmt.Println(days)
+
+  dayslice2 = append(dayslice1, "libur baru")
+  dayslice2[0] = "ups"
+  fmt.Println(dayslice2)
+  fmt.Println(days)
 
 }
