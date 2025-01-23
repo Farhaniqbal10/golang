7aea2ba7ffc91be175a8e99de6cd43f1568f69bf
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-23 12:56:19 +0700

    Create dataslice.go

---
+++ dataslice.go
@@ -0,0 +1,11 @@
+package main 
+import "fmt" 
+
+func main() {
+  names := [...] String {"farhan", "maulana", "iqbal", "mcgilly", "cuddy"}
+  slice := names[1:3] 
+
+  fmt.Println(slice[0])
+  fmt.Println(slice[1])
+
+}
